42f01bdf8fe5356beec3faf5ccddd6660d0d7bbd
unknown <victorha@gmail.com>
2019-07-13 21:12:26 -0400

    added carousel

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,3 +18,12 @@ $("#cancel").on("click", function() {
         $("#emailMessage").val("");
     }
 })
+
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+}
